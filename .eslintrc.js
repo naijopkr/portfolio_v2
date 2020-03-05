7b127bec151f1fea7a7453d137737d9e977cd483
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-03-05 14:50:19 -0500

    implements husky and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,36 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'plugin:import/errors',
-    'plugin:import/warnings',
+    'plugin:import/warnings'
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
     semi: ['error', 'never'],
     quotes: ['error', 'single'],
     'comma-dangle': ['error', 'only-multiline'],
-    'import/order': ['error', {
-      groups: [
-        ['builtin', 'external'],
-        ['sibling', 'parent'],
-        'index',
-      ],
-      'newlines-between': 'always'
-    }]
-  },
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], ['sibling', 'parent'], 'index'],
+        'newlines-between': 'always'
+      }
+    ]
+  }
 }
