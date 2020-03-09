dddfe14e699ba07ff8fbaa8d3d810a4d8feeecb8
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-03-09 11:25:26 -0500

    changes on i18n and adds locales

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,18 @@
-/* eslint import/no-dynamic-require: 0 */
-/* eslint global-require: 0 */
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import XHR from 'i18next-xhr-backend'
 import languageDetector from 'i18next-browser-languagedetector'
 
-const backend = async (url, _options, callback) => {
-  require(`./locales/${url}`)
-    .then(locale => {
-      callback(locale, { status: '200' })
-    })
-    .catch(() => {
-      callback(null, { status: '404' })
-    })
+import locales from './locales'
+
+const backend = (url, _options, callback) => {
+  const [ns, lng] = url.split('.')
+  try {
+    const locale = locales[ns][lng]
+    callback(locale, { status: '200' })
+  } catch (_err) {
+    callback(null, { status: '404' })
+  }
 }
 
 i18n.languages = ['en', 'pt', 'es']
@@ -27,7 +27,7 @@ i18n
       ajax: backend,
       allowMultiLoading: false,
       crossDomain: false,
-      loadPath: '{{ns}}/{{lng}}.json',
+      loadPath: '{{ns}}.{{lng}}',
       parse: data => data,
       withCredentials: false
     },
