af8486543b8f6d7c0d840f535749b615dc7bd7fc
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-04-16 10:06:28 -0500

    implements filters set

--- src/modules/projects/index.tsx
+++ src/modules/projects/index.tsx
@@ -6,10 +6,18 @@ import { IProject } from '../../data/interfaces'
 import Project from './components/project'
 import { ProjectsWrapper } from './styles'
 
+interface IFilters {
+  languages: Set<string>
+  frameworks: Set<string>
+  database: Set<string>
+}
+
 const Projects: React.FC = () => {
   const { t } = useTranslation('projects')
 
+  const [filters, setFilters] = useState<IFilters>()
   const [projects, setProjects] = useState<IProject[]>([])
+
   useEffect(() => {
     fetchProjects()
       .then(data => setProjects(data))
@@ -17,6 +25,46 @@ const Projects: React.FC = () => {
         setProjects([])
       })
   }, [])
+
+  useEffect(() => {
+    const langArray = projects.reduce((pv: string[], cv) => {
+      const { languages } = cv
+      if (!languages || !languages.length) {
+        return pv
+      }
+      return [...pv, ...languages]
+    }, [])
+
+    const frameworkArray = projects.reduce((pv: string[], cv) => {
+      const { frameworks } = cv
+      if (!frameworks || !frameworks.length) {
+        return pv
+      }
+      return [...pv, ...frameworks]
+    }, [])
+
+    const databaseArray = projects.reduce((pv: string[], cv) => {
+      const { database } = cv
+      if (!database || !database.length) {
+        return pv
+      }
+      return [...pv, ...database]
+    }, [])
+
+    const langSet = new Set(langArray)
+    const frameworkSet = new Set(frameworkArray)
+    const databaseSet = new Set(databaseArray)
+
+    setFilters({
+      languages: langSet,
+      frameworks: frameworkSet,
+      database: databaseSet
+    })
+  }, [projects])
+
+  useEffect(() => {
+    console.log(filters) // eslint-disable-line
+  }, [filters])
 
   const renderProjects = useCallback(() => {
     if (!projects.length) {
