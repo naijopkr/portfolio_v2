18e11b9e32dba08ebeaa7bd02119ed28cf77cb11
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-05-05 14:39:20 -0500

    adds articles data

--- src/data/interfaces.ts
+++ src/data/interfaces.ts
@@ -10,3 +10,32 @@ export interface IProject {
   database?: string[]
   updated_at: string
 }
+
+export interface IArticle {
+  body_markdown: string
+  canonical_url: string
+  comments_count: number
+  cover_image: string
+  description: string
+  id: number
+  page_views_count: number
+  path: string
+  positive_reactions_count: number
+  published: boolean
+  published_at: string
+  published_timestamp: string
+  slug: string
+  tag_list: string[]
+  title: string
+  type_of: string
+  url: string
+  user: {
+    github_username: string
+    name: string
+    profile_image: string
+    profile_image_90: string
+    twitter_username: string
+    username: string
+    website_url: string
+  }
+}
--- src/data/requests.ts
+++ src/data/requests.ts
@@ -1,6 +1,9 @@
-import { projects } from './projects.json'
-import { IProject } from './interfaces'
+import articles from './articles.json'
+import projects from './projects.json'
+import { IProject, IArticle } from './interfaces'
 
 type TFetch<T> = () => Promise<T>
 
 export const fetchProjects: TFetch<IProject[]> = async () => projects
+
+export const fetchArticles: TFetch<IArticle[]> = async () => articles
