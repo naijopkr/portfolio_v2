c2a8fa2ba956416c9ffa9068941dea9a9dbe079d
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-06-11 11:22:27 -0500

    implements LCM function

--- src/modules/playground/lib/prime-numbers.ts
+++ src/modules/playground/lib/prime-numbers.ts
@@ -1,3 +1,5 @@
+type TGetLCM = (n1: number, n2: number) => number
+
 export function* getNextPrime(): IterableIterator<number> {
   const primeNumbers = []
   let candidate = 2
@@ -24,4 +26,33 @@ export function* getNextPrime(): IterableIterator<number> {
 
     candidate += 1
   }
+}
+
+export const getLCM: TGetLCM = (num1: number, num2: number) => {
+  const nextPrime = getNextPrime()
+  const factors = []
+
+  let quotient1 = num1
+  let quotient2 = num2
+
+  while (quotient1 !== 1 || quotient2 !== 1) {
+    const factorCandidate = nextPrime.next().value
+
+    while (
+      quotient1 % factorCandidate === 0 ||
+      quotient2 % factorCandidate === 0
+    ) {
+      factors.push(factorCandidate)
+
+      if (quotient1 % factorCandidate === 0) {
+        quotient1 /= factorCandidate
+      }
+
+      if (quotient2 % factorCandidate === 0) {
+        quotient2 /= factorCandidate
+      }
+    }
+  }
+
+  return factors.reduce<number>((pv, cv) => pv * cv, 1)
 }
